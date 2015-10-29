674ef5b508ce29ad3b7fb3211541f1323d78e8a3
Kevin Howell <kevin@kahowell.net>
2015-10-28 21:44:12 -0400

    Bump version to 0.1.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 setup(
     name='sdl2-cffi',
     packages=['sdl2'],
-    version='0.1.2',
+    version='0.1.3',
     description='CFFI wrapper for SDL2',
     author='Kevin Howell',
     author_email='kevin@kahowell.net',
